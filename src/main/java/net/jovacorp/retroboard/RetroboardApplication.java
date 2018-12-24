52efb1662e8f4c8d317ee653643ea529842b37ca
Barnabas Jovanovics <jovanovics@lovelysystems.com>
2018-12-24 13:26:10 +0100

    initial commit

---
+++ src/main/java/net/jovacorp/retroboard/RetroboardApplication.java
@@ -0,0 +1,14 @@
+package net.jovacorp.retroboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroboardApplication.class, args);
+	}
+
+}
+
